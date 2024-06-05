e2d203812cf0c2eae6bdd6e0292f554650ecc9c7
Jaehyeon Kim <jakim@c2r16s4.42gyeongsan.kr>
2024-06-05 20:57:25 +0900

    first quick sort

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jakim <jakim@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 14:59:04 by jakim             #+#    #+#             */
-/*   Updated: 2024/05/31 21:36:51 by jakim            ###   ########.fr       */
+/*   Updated: 2024/06/05 16:41:28 by jakim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,15 @@ int	main(int argc, char *argv[])
 	b.stack = (int *)malloc(sizeof(int) * (a.size + 5));
 	null_guard(b.stack);
 	b.size = 0;
-	sort(&a, &b);
+	sort(&a, &b, a.size);
 	/*for (int i = 0; i < a.size; i++)
 	{
 		ft_printf("%d ",a.stack[i]);
+	}
+	ft_printf("\n\n");
+	for (int i = 0; i < b.size; i++)
+	{
+		ft_printf("%d ",b.stack[i]);
 	}*/
 	free(b.stack);
 	free(a.stack);
--- sort.c
+++ sort.c
@@ -6,12 +6,12 @@
 /*   By: jakim <jakim@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/31 20:31:21 by jakim             #+#    #+#             */
-/*   Updated: 2024/05/31 21:36:38 by jakim            ###   ########.fr       */
+/*   Updated: 2024/06/05 20:56:58 by jakim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
+/*
 static int	is_sorted(t_stack *a)
 {
 	int	i;
@@ -24,9 +24,178 @@ static int	is_sorted(t_stack *a)
 		i++;
 	}
 	return (1);
+}*/
+
+static t_stack	*stack_cpy(t_stack *a, int size)
+{
+	t_stack	*st;
+	int	i;
+	
+	i = 0;
+	st = (t_stack *)malloc(sizeof(t_stack));
+	st->size = size;
+	st->stack = (int *)malloc(sizeof(int) * (size + 5));
+	null_guard(st->stack);
+	while (i < size)
+	{
+		st->stack[i] = a->stack[i];
+		i++;
+	}
+	return (st);
 }
 
-static int	search_max(t_stack *a)
+static void	search_pivot(t_stack *st, int size, int *p1, int *p2)
+{
+	int	i;
+	int	k;
+	int	tmp;
+	t_stack *a;
+
+	a = stack_cpy(st, size);
+	i = 0;
+	while (i < size - 1)
+	{
+		k = 0;
+		while (k < size - 1 - i)
+		{
+			if (a->stack[k] > a->stack[k + 1])
+			{
+				tmp = a->stack[k];
+				a->stack[k] = a->stack[k + 1];
+				a->stack[k + 1] = tmp;
+			}
+			k++;
+		}
+		i++;
+	}
+	*p1 = a->stack[size / 3];
+	*p2 = a->stack[size / 3 * 2];
+	free(a->stack);
+	free(a);
+}
+
+static void	mini_sort(t_stack *a, int size)
+{
+	if (size == 1)
+		return ;
+	else if (size == 2)
+	{
+		if (a->stack[0] > a->stack[1])
+			sa(a);
+	}
+	else if (size == 3)
+	{
+		if (a->stack[0] < a->stack[1] && a->stack[0] < a->stack[2] && a->stack[1] > a->stack[2])
+		{
+			ra(a);
+			sa(a);
+			rra(a);
+		}
+		else if (a->stack[0] > a->stack[1] && a->stack[0] < a->stack[2] && a->stack[1] < a->stack[2])
+			sa(a);
+		else if (a->stack[0] < a->stack[1] && a->stack[0] > a->stack[2] && a->stack[1] > a->stack[2])
+		{
+			ra(a);
+			sa(a);
+			rra(a);
+			sa(a);
+		}
+		else if (a->stack[0] > a->stack[1] && a->stack[0] > a->stack[2] && a->stack[1] < a->stack[2])
+		{
+			sa(a);
+			ra(a);
+			sa(a);
+			rra(a);
+		}
+		else if (a->stack[0] > a->stack[1] && a->stack[0] > a->stack[2] && a->stack[1] > a->stack[2])
+		{
+			sa(a);
+			ra(a);
+			sa(a);
+			rra(a);
+			sa(a);
+		}
+	}
+}
+
+void	sort(t_stack *a, t_stack *b, int size)
+{
+	int	r;
+	int	s;
+	int	m;
+	int	i;
+	int p1;
+	int p2;
+
+	i = 0;
+	r = 0;
+	s = 0;
+	m = 0;
+	if (size <= 3)
+	{
+		mini_sort(a, size);
+		return ;
+	}
+	search_pivot(a, size, &p1, &p2);
+	while (i < size)
+	{
+		if (a->stack[0] <= p1)
+		{
+			pb(a, b);
+			rb(b);
+			s++;
+		}
+		else if (a->stack[0] <= p2)
+		{
+			pb(a, b);
+			m++;
+		}
+		else
+		{
+			ra(a);
+			r++;
+		}
+		i++;
+	}
+	i = 0;
+	if (a->size != r)
+	{
+		while (i < r)
+		{
+			rra(a);
+			i++;
+		}
+	}
+	sort(a, b, r);
+	i = 0;
+	while (i < m)
+	{
+		pa(a, b);
+		i++;
+	}
+	sort(a, b, m);
+	i = 0;
+	if (s == b->size)
+	{
+		while (i < s)
+		{
+			pa(a, b);
+			i++;
+		}
+	}
+	else
+	{
+		while (i < s)
+		{
+			rrb(b);
+			pa(a, b);
+			i++;
+		}
+	}
+	sort(a, b, s);
+}
+
+/*static int	search_max(t_stack *a)
 {
 	int	i;
 	int	max;
@@ -44,52 +213,34 @@ static int	search_max(t_stack *a)
 		i++;
 	}
 	return (index);
-}
+}*/
+/*
 
-static void	prepare(t_stack *a, t_stack *b)
+
+void	first_sort(t_stack *a, t_stack *b)
 {
 	int	i;
-	int	now;
-	
-	i = 0;
-	now = a->size;
-	while (i < now)
-	{
-		pb(a, b);
-		i++;
-	}
-}
-
-void	sort(t_stack *a, t_stack *b)
-{
-	int	i;
+	int	m;
 	int	k;
-	int	index;
-	int	now;
 
-	prepare(a, b);
 	i = 0;
-	now = b->size;
-	while (i < now)
+	m = stack_cpy(a);
+	ft_printf("%d",m);
+	k = a->size;
+	while (i < k)
 	{
-		if(!is_sorted(b))
-		{
-			index = search_max(b);
-			k = 0;
-			if (index < b->size / 2)
-				while (k < index)
-				{
-					rb(b);
-					k++;
-				}
-			else
-				while (k < b->size - index)
-				{
-					rrb(b);
-					k++;
-				}
-		}
-		pa(a, b);
+		if (a->stack[0] <= m)
+			pb(a, b);
+		else
+			ra(a);
 		i++;
 	}
 }
+
+void	second_sort(t_stack *a, t_stack *b)
+{
+	while (b->size)
+	{
+		
+	}
+}*/
